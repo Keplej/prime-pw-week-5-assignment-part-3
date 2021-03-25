53d9469a88f07083b6d8e8c46659df84c269bedb
Johnny Keple <Johnny.Keple@gmail.com>
2021-03-25 11:25:37 -0500

    Added Collection & function

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+
+//Creating a variable
+let collection = [];
+
+//Creating a function
+function addToCollection(title, artist, yearPublished) {
+  let album = {
+    title = title,
+    artist = artist,
+    yearPublished = yearPublished
+  }
+  collection.push(album);
+}
